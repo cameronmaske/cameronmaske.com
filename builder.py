2c1e637e08e67cd865a3c55d5d5fa70e9613a09f
cameronmaske <cameronmaske@gmail.com>
2012-12-28 23:46:04 +0100

    Basic flask site in place. Need to sort out the indivudal pages

--- builder.py
+++ builder.py
@@ -1,7 +1,9 @@
 #Based on this tutorial - https://nicolas.perriault.net/code/2012/dead-easy-yet-powerful-static-website-generator-with-flask/
+import sys
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -10,14 +12,25 @@ FLATPAGES_EXTENSION = '.md'
 app = Flask(__name__)
 app.config.from_object(__name__)
 pages = FlatPages(app)
+freezer = Freezer(app)
 
+
+#The homepage: e.g. cameronmaske.com/
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html', pages=pages)
 
+
+#Individual blog articles. e.g. cameronmaske.com/what-is-wrong-with-the-world
 @app.route('/<path:path>/')
 def page(path):
-    return pages.get_or_404(path).html
+    page = pages.get_or_404(path).html
+    return render_template('post.html', page=page)
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    # $ python builder.py build : generates the static site.
+    if len(sys.argv) > 1 and sys.argv[1] == 'build':
+        freezer.freeze()
+    # $ python builder.py : serves the site locally for dev.e
+    else:
+        app.run(port=8000)
---
+++ pages/another-test-page.md
@@ -0,0 +1,8 @@
+title: Venenatis Fringilla Egestas Tellus
+published: 2012-03-04
+topic: LIFE
+intro: Aenean lacinia bibendum nulla sed consectetur. Cras mattis consectetur purus sit amet fermentum. Donec ullamcorper nulla non metus auctor fringilla.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas sed diam eget risus varius blandit sit amet non magna.
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus.
